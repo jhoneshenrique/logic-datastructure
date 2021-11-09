a04b6346c207c727ed244cbebf33659c89e5d8d2
Jhones <jhoneshenriquedasilva@gmail.com>
2021-11-09 10:48:52 -0300

    QuickSort study

---
+++ src/main/java/arrays_quicksort/ArraySorting.java
@@ -0,0 +1,43 @@
+package arrays_quicksort;
+
+import java.util.Arrays;
+
+public class ArraySorting {
+    public static void main(String[] args) {
+        int numbers[] = {12,3,2,9,5,32};
+
+        quickSort(numbers,0,numbers.length-1);
+
+        Arrays.stream(numbers).forEach(System.out::println);
+    }
+
+    public static void quickSort(int arr[], int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(arr, begin, end);
+
+            quickSort(arr, begin, partitionIndex-1);
+            quickSort(arr, partitionIndex+1, end);
+        }
+    }
+
+    private static int partition(int arr[], int begin, int end) {
+        int pivot = arr[end];
+        int i = (begin-1);
+
+        for (int j = begin; j < end; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+
+                int swapTemp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = swapTemp;
+            }
+        }
+
+        int swapTemp = arr[i+1];
+        arr[i+1] = arr[end];
+        arr[end] = swapTemp;
+
+        return i+1;
+    }
+}
